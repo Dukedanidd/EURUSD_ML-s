3c1429a2145b19c200de8218852d501b3c368f52
Duke <jd.daniel.contact@gmail.com>
2025-01-20 21:15:26 -0600

    Enhance data preprocessing for prediction model by adding scaling and training data length calculation

--- Model.py
+++ Model.py
@@ -20,3 +20,21 @@ plt.xlabel('Date', fontsize=16)
 plt.ylabel('Close Price USD ($)', fontsize=16)
 plt.title('Close Price History', fontsize=16)
 plt.show()
+
+# Create a new df with only the 'Close' column, ensuring the column exists
+data = df[['Close']]
+
+# Convert the df to a numpy array
+dataset = data.values
+
+# Get the number of rows to train the model on
+training_data_len = math.ceil(len(dataset) * .8)
+
+training_data_len
+
+# Scale the data
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(dataset)
+
+scaled_data
+
